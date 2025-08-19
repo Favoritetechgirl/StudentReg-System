c2fa0837ae10f2bf97ba4c5bed1182db60792c79
Mariam Jelili <maryamjelili96@gmail.com>
2025-08-19 11:33:51 +0100

    first commit

---
+++ src/main/java/mariam/studentregsystem/StudentRegSystemApplication.java
@@ -0,0 +1,13 @@
+package mariam.studentregsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRegSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mariam/studentregsystem/StudentRegSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package mariam.studentregsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
